bb80d5be8d171f9e96ab33c4431c8b0fdffbfd59
thai <nguyenvanthai0212@gmail.com>
2021-06-22 02:09:12 +0700

    decrease network size

--- ddqn.py
+++ ddqn.py
@@ -5,10 +5,10 @@ from tensorflow.keras.layers import LeakyReLU
 class DDDQN(tf.keras.Model):
     def __init__(self, action_space_num=(56)):
         super(DDDQN, self).__init__()
-        self.d1 = tf.keras.layers.Dense(64, activation=LeakyReLU(alpha=0.1))
-        self.d2 = tf.keras.layers.Dense(64, activation=LeakyReLU(alpha=0.1))
-        self.v = tf.keras.layers.Dense(1, activation=None)
-        self.a = tf.keras.layers.Dense(action_space_num, activation=None)
+        self.d1 = tf.keras.layers.Dense(64, activation=LeakyReLU(alpha=0.1), kernel_initializer='he_uniform')
+        self.d2 = tf.keras.layers.Dense(64, activation=LeakyReLU(alpha=0.1), kernel_initializer='he_uniform')
+        self.v = tf.keras.layers.Dense(1, activation=None, kernel_initializer='he_uniform')
+        self.a = tf.keras.layers.Dense(action_space_num, activation=None, kernel_initializer='he_uniform')
 
     def call(self, input_data):
         x = self.d1(input_data)
@@ -35,10 +35,10 @@ def advantage(model, state):
 class PERDDDQN(tf.keras.Model):
     def __init__(self, action_space_num=(56)):
         super(PERDDDQN, self).__init__()
-        self.d1 = tf.keras.layers.Dense(64, activation=LeakyReLU(alpha=0.1))
-        self.d2 = tf.keras.layers.Dense(64, activation=LeakyReLU(alpha=0.1))
-        self.v = tf.keras.layers.Dense(1, activation=None)
-        self.a = tf.keras.layers.Dense(action_space_num, activation=None)
+        self.d1 = tf.keras.layers.Dense(64, activation=LeakyReLU(alpha=0.1), kernel_initializer='he_uniform')
+        self.d2 = tf.keras.layers.Dense(64, activation=LeakyReLU(alpha=0.1), kernel_initializer='he_uniform')
+        self.v = tf.keras.layers.Dense(1, activation=None, kernel_initializer='he_uniform')
+        self.a = tf.keras.layers.Dense(action_space_num, activation=None, kernel_initializer='he_uniform')
 
     def call(self, input):
         x = self.d1(input)
